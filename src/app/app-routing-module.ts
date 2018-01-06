22b9344a3e72e4f42fee93286412e90f60e41fb8
Justin Be <jrkbender@gmail.com>
2018-01-06 01:30:33 -0500

    added a carousel

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,12 +4,16 @@ import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { CanopyComponent } from './canopy/canopy.component';
+import { ServicesComponent } from './services/services.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 const routes = [
     { path: 'home', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'canopy', component: CanopyComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'blog', component: BlogComponent },
     { path: '**', redirectTo: '/home'}
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './contact/contact.component';
 import { CanopyComponent } from './canopy/canopy.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { BlogComponent } from './blog/blog.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { CarouselComponent } from './carousel/carousel.component';
     HeaderComponent,
     ContactComponent,
     CanopyComponent,
-    CarouselComponent
+    CarouselComponent,
+    BlogComponent,
+    ServicesComponent
   ],
   imports: [
     NgbModule.forRoot(),
